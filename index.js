06acb45eded71e0349c9d8059c9b3eeb6ff4ef3f
HarshdipD <harshdipsdeogan@gmail.com>
2019-08-29 19:32:04 -0400

    ddd

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 // go to localhost 3003 --> if it doesnt work --> change to any number
-var server = app.listen(3003, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
     var host = server.address().address
     var port = server.address().port
-    console.log("start");
+    console.log("start!!!!");
   
   });
 
